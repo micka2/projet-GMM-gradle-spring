235cefe77a2ed6c52f86cbfc4493197589c7a09d
Mickael Duranteau <mickael.duranteau@gmail.com>
2017-01-19 11:56:59 +0100

    ajout de la dépendence spring-security-config

--- src/main/java/fr/imie/gmm/repository/StudentRepo.java
+++ src/main/java/fr/imie/gmm/repository/StudentRepo.java
@@ -1,9 +1,11 @@
 package fr.imie.gmm.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import fr.imie.gmm.entities.Student;
 
+@Repository
 public interface StudentRepo extends CrudRepository<Student, Long>{
 
 }
