9781d04303f4a3fee1ea0a9ca210f96cdae4e908
Guénaëlle Deschamps <guenaelle.deschamps@gmail.com>
2017-01-24 15:56:53 +0100

    ajout changement mickael

--- src/main/java/fr/imie/gmm/Main.java
+++ src/main/java/fr/imie/gmm/Main.java
@@ -5,14 +5,10 @@ package fr.imie.gmm;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import fr.imie.gmm.entities.User;
 import fr.imie.gmm.repositories.UserRepository;
 
 @SpringBootApplication
