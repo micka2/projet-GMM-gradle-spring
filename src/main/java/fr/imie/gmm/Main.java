e46e242407fa0bbf9b46fd93bc23af91c79a6eae
Guénaëlle Deschamps <guenaelle.deschamps@gmail.com>
2017-01-24 10:09:43 +0100

    changement de mot de passe

--- src/main/java/fr/imie/gmm/Main.java
+++ src/main/java/fr/imie/gmm/Main.java
@@ -2,15 +2,10 @@ package fr.imie.gmm;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import fr.imie.gmm.entities.User;
 import fr.imie.gmm.repositories.UserRepository;
 
 @SpringBootApplication
@@ -20,10 +15,6 @@ public class Main implements CommandLineRunner
 	@Autowired
 
 	private UserRepository repository;
-
-
-
-
 
 
 	
