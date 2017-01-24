c7291bf9a18af84032526e416d4fd0e5eeaaa755
Guénaëlle Deschamps <guenaelle.deschamps@gmail.com>
2017-01-24 15:28:02 +0100

    resolution de conflits

--- src/main/java/fr/imie/gmm/Main.java
+++ src/main/java/fr/imie/gmm/Main.java
@@ -2,16 +2,10 @@ package fr.imie.gmm;
 
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-=======
->>>>>>> micka
-=======
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-
-
->>>>>>> c7a563534841477adce7c67267f7a6e6ba513fe9
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -29,34 +23,12 @@ public class Main implements CommandLineRunner
 
 	private UserRepository repository;
 
-
-
-
-
-
-
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
 
-
-
 	
-	/*@Override
-	public void run(String...args) throws Exception {
-			User user = new User ("toto.toto@toto.com", "pass");
-		user.setFirstname("Toto");
-			 this.repository.save(user);
-			 }*/
-
-	
-	
-		@Override
-		public void run(String...args) throws Exception {
-			
-		}
-
 }	
 			
 
