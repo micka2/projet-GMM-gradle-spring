78b845555cafacada7d453ec84bc2fec9dadcf96
Guénaëlle Deschamps <guenaelle.deschamps@gmail.com>
2017-01-26 16:47:59 +0100

    modification du teacherController, fonction ajout de sujet

--- src/main/java/fr/imie/gmm/controllers/TeacherController.java
+++ src/main/java/fr/imie/gmm/controllers/TeacherController.java
@@ -31,7 +31,7 @@ public class TeacherController {
 	/**
      * Size of a byte buffer to read/write file.
      */
-    private static final int BUFFER_SIZE = 4096;
+  //  private static final int BUFFER_SIZE = 4096;
              
     /**
      * Path of the file to be downloaded, relative to application's directory.
@@ -48,7 +48,15 @@ public class TeacherController {
   	  
   	  return "teacher_view1";
     }
-	
+
+    /////////cliquer sur le bouton creer un sujet///////////
+    
+    @RequestMapping(method=RequestMethod.GET, path="/teacher/creationdesujet")
+    public String creationsubject (Model model){
+  	  
+  	  return "teacher_view2";
+    }
+    
 
 	/////////////////////////////////////////////////////////////////////////
 	//////Recherche de promotion et affichage des sujets correspondants//////
@@ -113,47 +121,89 @@ public class TeacherController {
 //
 //	}
 	
-	@RequestMapping(method=RequestMethod.GET, path="/recherchepromotion")
-	public String checkGrade (
-			@RequestParam(name="Name")
-			String Name,
-			Model model){
-		
-		Grade gradeRepo = new Grade(Name);
-		Grade grade = this.gradeRepo.findByName(Name);
-		
-		if(grade==null){
-			return ("teacher_view1");
-		}
-		else if ((grade!=null)&&(grade.getName().equals(Name))){
-			model.addAttribute("NamePromo", grade);
-		return ("teacher_view1");
-	}
-		else{
-			model.addAttribute("fail", true);
-			return "teacher_view1";
-	}		
 	
-	}
+
+	
+	
+///////////////////////clique sur le bouton modifier/////////////////
+	
+//	
+//	@RequestMapping(method=RequestMethod.POST, path="teacher/affichersujet")
+//		public String recupsujet( 
+//				@RequestParam (name="title")
+//				String title,
+//				Model model){
+//		Subject subject = this.subjectRepo.findByTitle(title);
+//	if ((subject!= null)){
+//		return "teacher_view2";
+//	}else{
+//		model.addAttribute("fail", true);
+//		return "teacher_view1";
+//	}
+//}
+//	
+//	@RequestMapping(method=RequestMethod.GET, path="/teacher/modificationsujet")
+//		public String sendmodifpage (
+//				Model model){
+//		return "teacher_view2";
+//	}
+//	
+//////////////////////////poster un nouveau sujet////////////////////
+	
+	
+//	@RequestMapping(method=RequestMethod.GET, path="/recherchepromotion")
+//	public String checkGrade (
+//			@RequestParam(name="Name")
+//			String Name,
+//			Model model){
+//		
+//		Grade gradeRepo = new Grade(Name);
+//		Grade grade = this.gradeRepo.findByName(Name);
+//		
+//	
+//		if ((grade!=null)&&(grade.getName().equals(Name))){
+//			model.addAttribute("NamePromo", grade);
+//		return ("teacher_view1");
+//	}
+//		else{
+//			model.addAttribute("fail", true);
+//			return "teacher_view1";
+//	}		
+//	
+//	}
 	
 	
 	
 		////////////////////////////////////////////////
 		///////creer un nouveau sujet//////////////////
-	@Autowired
+	
 	protected SubjectRepository subjectRepo;
 	
-	@RequestMapping("/createSubject")
-	@ResponseBody
-	public String createSubject(String title, Date deadline, String description, String constraint){
-	 try {
-		Subject subject = new Subject (title, deadline, description, constraint);
-		subjectRepo.save(subject);
-	 }
-	 catch (Exception ex) {
-	      return "Error creating the subject: " + ex.toString();
+	@RequestMapping(method=RequestMethod.POST, path="/createSubject")
+	public String createSubject(
+			@RequestParam (name="title")
+			String title,
+			@RequestParam (name="deadline")
+			Date deadline,
+			@RequestParam (name="description")
+			String description,
+			@RequestParam (name="constraint")
+			String constraint,
+			Model model){
+	
+		Subject subject = this.subjectRepo.findByTitle(title);
+
+	 
+		if ((title!= null)&&(deadline!=null)&&(description!=null)){
+			model.addAttribute("subject", subject);
+			model.addAttribute("succes", true);
+			subjectRepo.save(subject);
+	      return "teacher_view1";
 	    }
-	    return "Subject succesfully created!";
+	else{
+		model.addAttribute("fail2",true);
+	    return "teacher_view2";
+}
 	  }
 		
 	
