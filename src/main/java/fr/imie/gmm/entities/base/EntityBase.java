9c734f1cb59d08427d6bcee0d2657bbb285371d4
Guénaëlle Deschamps <guenaelle.deschamps@gmail.com>
2017-01-26 11:36:32 +0100

    progress

--- src/main/java/fr/imie/gmm/entities/base/EntityBase.java
+++ src/main/java/fr/imie/gmm/entities/base/EntityBase.java
@@ -1,7 +1,5 @@
 package fr.imie.gmm.entities.base;
 
-import java.io.Serializable;
-
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
